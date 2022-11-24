607dfe0e1b244ec245902a62f07e20201a674f46
ismilearefin <arefinismile@gmail.com>
2022-11-24 13:41:06 +0600

    MongoDB setUp

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const cors = require('cors');
+const app = express();
 require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// Middleware..
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.y3njyog.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run(){
+
+    try{
+
+
+        
+
+
+    }
+    finally{
+
+    }
+
+}
+run().catch((err)=>console.log(err))
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res)=> {
+    res.send('server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`Listing to port ${port}`)
+});
